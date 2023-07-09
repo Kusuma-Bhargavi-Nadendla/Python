6b2c6c9d7059a746b0423f5d632777892dbf865f
Kusuma Bhargavi Nadendla <130216187+Kusuma-Bhargavi-Nadendla@users.noreply.github.com>
2023-07-09 12:57:02 +0530

    Update 8-puzzle-dfs.py

--- 8-puzzle-dfs.py
+++ 8-puzzle-dfs.py
@@ -1,9 +1,7 @@
 def child_node(in_sta,ac):
+    #actions={1:'left',2:'right',3:'up',4:'down'}
     child=[]
-    for i in range(len(in_sta)):
-        if in_sta[i]==0:
-            bl_loc=i
-            break
+    bl_loc=in_sta.index(0)
     t=bl_loc
     if(ac==1):
         if bl_loc==0 or bl_loc==3 or bl_loc==6:
@@ -31,44 +29,52 @@ def child_node(in_sta,ac):
             bl_loc+=3
     if f==1:
         child=[]+in_sta
-        #in_sta[bl_loc],in_sta[t]=in_sta[t],in_sta[bl_loc]
         child[bl_loc],child[t]=child[t],child[bl_loc]
         return child
     else:
         return [0]
   
-
-def bfs(problem):
+frontier=[]
+explored=[]
+def dfs(problem):
     node=problem['initial']
-    path_cost=0
+    
     if node==problem['goal']:
         return node 
-    frontier=[node]
-    explored=[]
+    frontier.append(node)
     while(node!=problem['goal']):
+        
         if len(frontier)==0:
             return 'failure'
-        node=frontier[0]
-        frontier=frontier[1:]
+        
+        node=frontier.pop()
+        if node in explored:
+            return 'failure'
         explored.append(node)
-        for ac in range(len(problem['actions'])):
-            child=child_node(node,ac+1)
-            if child==[0]:
+        print(node)
+        for ac in problem['actions']:
+            child=child_node(node,ac)
+            if child==[0] or child==node:
               continue 
             if (child not in frontier) and (child not in explored):
                 if child==problem['goal']:
                     return child 
-                frontier.append(child)
-                print(child)
-        
-        
+                frontier.append(node)
+                problem={'initial':child,'goal':goal_sta,'actions':[1,2,3,4]}
+                res=dfs(problem)
+                if res!='failure':
+                    return res 
+                else:
+                    continue
+
+#in_sta=[2, 8, 3, 1, 6, 4, 7, 0, 5]
+#goal_sta=[1,2,3,4,8,0,7,5,6]
+#There is no guarantee for solution using DFS 
 print('Enter initial state:')
 in_sta=[int(x) for x in input().split()]
-
 print('Enter goal state of puzzle')
 goal_sta=[int(x) for x in input().split()]
-actions={1:'left',2:'right',3:'up',4:'down'}
 problem={'initial':in_sta,'goal':goal_sta,'actions':[1,2,3,4]}
-
-res=bfs(problem)
+res=dfs(problem)
 print('Goal state reached:',res)
+
